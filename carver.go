564c38c0ec575e8751399c1b91eb36a4e790d650
kfcampbell <Keegan.Campbell@microsoft.com>
2019-12-26 15:56:30 -0800

    Moved to actual rendering and not debug printing.

--- carver.go
+++ carver.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"image/color"
 	"log"
 	"strings"
 
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/inpututil"
+	"github.com/hajimehoshi/ebiten/text"
 )
 
 var (
-	text    = "Begin your masterpiece, or maybe just a shopping list:\n"
-	counter = 0
+	userText   = "Begin your masterpiece, or maybe just a shopping list:\n"
+	counter    = 0
+	tt         *truetype.Font
+	normalFont font.Face
+	dpi        = 72
 )
 
-type Game struct{}
+type Game struct {
+	boardImage *ebiten.Image
+}
 
 // repeatingKeyPressed return true when key is pressed considering the repeat state.
 func repeatingKeyPressed(key ebiten.Key) bool {
@@ -35,6 +45,7 @@ func repeatingKeyPressed(key ebiten.Key) bool {
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+
 	// outsideWidth and outsideHeight are set on line 85 by default
 	// they change when the user resizes the window
 	var newWidth int = int(float64(outsideWidth) * 0.6)
@@ -48,23 +59,23 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	// Add a string from InputChars, that returns string input by users.
 	// Note that InputChars result changes every frame, so you need to call this
 	// every frame.
-	text += string(ebiten.InputChars())
+	userText += string(ebiten.InputChars())
 
 	// Adjust the string to be at most 10 lines.
-	ss := strings.Split(text, "\n")
+	ss := strings.Split(userText, "\n")
 	if len(ss) > 10 {
-		text = strings.Join(ss[len(ss)-10:], "\n")
+		userText = strings.Join(ss[len(ss)-10:], "\n")
 	}
 
 	// If the enter key is pressed, add a line break.
 	if repeatingKeyPressed(ebiten.KeyEnter) || repeatingKeyPressed(ebiten.KeyKPEnter) {
-		text += "\n"
+		userText += "\n"
 	}
 
 	// If the backspace key is pressed, remove one character.
 	if repeatingKeyPressed(ebiten.KeyBackspace) {
-		if len(text) >= 1 {
-			text = text[:len(text)-1]
+		if len(userText) >= 1 {
+			userText = userText[:len(userText)-1]
 		}
 	}
 
@@ -76,13 +87,37 @@ func (g *Game) Update(screen *ebiten.Image) error {
 		return nil
 	}
 
-	// Write your game's rendering.
-	t := text
+	// append blinking cursor to text
+	t := userText
 	if counter%60 < 30 {
 		t += "_"
 	}
-	ebitenutil.DebugPrint(screen, t)
+
+	if g.boardImage == nil {
+		w, h := 1280, 960
+		g.boardImage, _ = ebiten.NewImage(w, h, ebiten.FilterDefault)
+	}
+	screen.Fill(color.Black)
+
+	slices := strings.Split(t, "\n")
+
+	for i := 0; i < len(slices); i++ {
+		text.Draw(screen, slices[i], normalFont, 15, 15+(20*i), color.White)
+	}
+
 	return nil
+}
+
+func init() {
+	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	normalFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    12,
+		DPI:     float64(dpi),
+		Hinting: font.HintingFull,
+	})
 }
 
 func main() {
--- todo.md
+++ todo.md
@@ -1,6 +1,8 @@
 # todo
 
 - adjustable text size
+- movable cursor
+    - don't lose data off the page or greater than 10 lines
 - word wrap
 - save file
 - load file
